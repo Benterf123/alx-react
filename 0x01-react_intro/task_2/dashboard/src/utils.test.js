0c05d440cd932c7ae56f367e3b794ae74bc7acea
Benterf123 <faithbenter@gmail.com>
2023-04-19 19:57:41 +0000

    corrections

---
+++ 0x01-react_intro/task_2/dashboard/src/utils.test.js
@@ -0,0 +1,31 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils.js';
+
+// Test for getFullYear function
+describe('getFullYear', () => {
+  it('should return the current year', () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toEqual(currentYear);
+  });
+});
+
+// Test for getFooterCopy function
+describe('getFooterCopy', () => {
+  it('should return "Holberton School" when the argument is true', () => {
+    expect(getFooterCopy(true)).toEqual('Holberton School');
+  });
+
+  it('should return "Holberton School main dashboard" when the argument is false', () => {
+    expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+  });
+});
+
+// Test for getLatestNotification function
+describe('getLatestNotification', () => {
+  it('should return a string', () => {
+    expect(typeof getLatestNotification()).toEqual('string');
+  });
+
+  it('should return a string containing the word "Urgent"', () => {
+    expect(getLatestNotification()).toContain('Urgent');
+  });
+});
